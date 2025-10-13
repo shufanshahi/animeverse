c109cbb8aecd1b30e982bf09eb0fa49bd6828e40
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2025-10-13 14:21:59 +0600

    Added auth feature initialization

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AnimeHub',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.purple,
