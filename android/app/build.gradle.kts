86d1993a2d64d64a1bcd7ab88064ad655336b8b4
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2025-10-14 15:31:44 +0600

    Firebase connect, And basic Login, sigup, reset password added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -8,7 +11,7 @@ plugins {
 android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    namespace = "com.android.application"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
